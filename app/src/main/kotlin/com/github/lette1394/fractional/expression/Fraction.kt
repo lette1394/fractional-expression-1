bcc9251be3e389c20cde92e92f60040331cb1f05
lette1394 <lette1394@gmail.com>
2022-02-23 03:27:44 +0900

    green: 11/23

--- app/src/main/kotlin/com/github/lette1394/fractional/expression/Fraction.kt
+++ app/src/main/kotlin/com/github/lette1394/fractional/expression/Fraction.kt
@@ -1,15 +1,15 @@
 package com.github.lette1394.fractional.expression
 
 import com.github.lette1394.fractional.expression.Fits.thenDown
+import kotlin.math.max
 
 
 class Fraction(private val expression: String) {
 
     fun asString(): String {
-
-        val delimiter = "---"
         val numerator = expression.split("/")[0]
         val denominator = expression.split("/")[1]
+        val delimiter = "-".repeat(max(numerator.length, denominator.length) + 2)
 
         return numerator thenDown delimiter thenDown denominator
     }
