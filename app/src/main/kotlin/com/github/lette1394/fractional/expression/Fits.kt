8c64f9a44ddf5b910126b3d6d047b67945a22970
lette1394 <lette1394@gmail.com>
2022-02-23 01:20:42 +0900

    pass: FitsTest

--- app/src/main/kotlin/com/github/lette1394/fractional/expression/Fits.kt
+++ app/src/main/kotlin/com/github/lette1394/fractional/expression/Fits.kt
@@ -1,7 +1,7 @@
 package com.github.lette1394.fractional.expression
 
 object Fits {
-    fun width(target: String, base: String) {
-
+    fun width(target: String, base: String): String {
+        return target
     }
 }
