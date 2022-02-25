535e0963d6b50ab7e77d6d1e6df83692aa8c3e6c
lette1394 <lette1394@gmail.com>
2022-02-25 23:55:36 +0900

    red: PasswordTest

--- password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordTest.kt
+++ password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordTest.kt
@@ -1,9 +1,25 @@
 package com.github.lette1394.password.validator
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 
 class PasswordTest : FreeSpec({
-    ""
+    "Password must be at least 8 characters - 1" {
+        shouldNotThrowAny {
+            Password("12345678")
+        }
+    }
+
+    "Password must be at least 8 characters - 2" {
+        shouldThrowAny {
+            Password("1234567")
+        }
+    }
+
+    "Password must be at least 8 characters - 3" {
+        shouldThrowAny {
+            Password("")
+        }
+    }
 })
