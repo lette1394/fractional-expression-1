cf40b36f02b115bcd3d9f97b4d1d63dda3e69563
lette1394 <lette1394@gmail.com>
2022-02-26 04:46:09 +0900

    refactor: assert all

--- password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordMustBeAtLeast8CharactersTest.kt
+++ password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordMustBeAtLeast8CharactersTest.kt
@@ -6,24 +6,29 @@ import com.github.lette1394.password.validator.Reasons.Companion.reasons
 import com.github.lette1394.password.validator.Reasons.Reason.PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertAll
 
 class PasswordMustBeAtLeast8CharactersTest : FreeSpec({
     val subject = PasswordMustBeAtLeast8Characters()
 
     "pass" {
         val expected = Right(Unit)
-        subject.matches("12345678") shouldBe expected
-        subject.matches("abcdefgh") shouldBe expected
-        subject.matches("58gvi13n") shouldBe expected
-        subject.matches("00000000") shouldBe expected
-        subject.matches("000000000") shouldBe expected
+        assertAll({
+            subject.matches("12345678") shouldBe expected
+            subject.matches("abcdefgh") shouldBe expected
+            subject.matches("58gvi13n") shouldBe expected
+            subject.matches("00000000") shouldBe expected
+            subject.matches("000000000") shouldBe expected
+        })
     }
 
     "fail" {
         val expected = Left(reasons(PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS))
-        subject.matches("") shouldBe expected
-        subject.matches("1") shouldBe expected
-        subject.matches("1234567") shouldBe expected
-        subject.matches("abcdedf") shouldBe expected
+        assertAll({
+            subject.matches("") shouldBe expected
+            subject.matches("1") shouldBe expected
+            subject.matches("1234567") shouldBe expected
+            subject.matches("abcdedf") shouldBe expected
+        })
     }
 })
