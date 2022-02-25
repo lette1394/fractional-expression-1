2792552c5f89f625af42497d3479b0e04e01c65a
lette1394 <lette1394@gmail.com>
2022-02-26 04:36:05 +0900

    mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ subprojects {
         testImplementation("io.kotest:kotest-runner-junit5:5.1.0")
         testImplementation("io.kotest:kotest-assertions-core:5.1.0")
         testImplementation("io.kotest:kotest-property:5.1.0")
+
+        testImplementation("io.mockk:mockk:1.12.2")
     }
 
     application {
