e9ccb6eb513ca5c3a3af364605a631913e2f0e99
lette1394 <lette1394@gmail.com>
2022-02-26 00:48:53 +0900

    red: password must contain at least one capital letter

--- password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordTest.kt
+++ password-validator/src/test/kotlin/com/github/lette1394/password/validator/PasswordTest.kt
@@ -4,6 +4,7 @@ import arrow.core.Either.Left
 import arrow.core.Either.Right
 import com.github.lette1394.password.validator.Reasons.Companion.reasons
 import com.github.lette1394.password.validator.Reasons.Reason.PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS
+import com.github.lette1394.password.validator.Reasons.Reason.PASSWORD_MUST_CONTAIN_AT_LEAST_ONE_CAPITAL_LETTER
 import com.github.lette1394.password.validator.Reasons.Reason.THE_PASSWORD_MUST_CONTAIN_AT_LEAST_2_NUMBERS
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
@@ -32,5 +33,9 @@ class PasswordTest : FreeSpec({
 
     "The password must contain at least 2 numbers - 2" {
         Password.create("abcdefgh") shouldBe Left(reasons(THE_PASSWORD_MUST_CONTAIN_AT_LEAST_2_NUMBERS))
+    }
+
+    "password must contain at least one capital letter - 1" {
+        Password.create("abcdefgh1234") shouldBe Left(reasons(PASSWORD_MUST_CONTAIN_AT_LEAST_ONE_CAPITAL_LETTER))
     }
 })
