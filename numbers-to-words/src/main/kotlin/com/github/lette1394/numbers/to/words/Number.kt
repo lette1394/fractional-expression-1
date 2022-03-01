9ffebbb8189d40dd9abfe965f0123eb931d9e045
lette1394 <lette1394@gmail.com>
2022-03-01 21:52:05 +0900

    green: 1 digit

--- numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
+++ numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
@@ -7,6 +7,13 @@ class Number(private val value: Long) {
             0L -> "영"
             1L -> "일"
             2L -> "이"
+            3L -> "삼"
+            4L -> "사"
+            5L -> "오"
+            6L -> "육"
+            7L -> "칠"
+            8L -> "팔"
+            9L -> "구"
             else -> TODO()
         }
     }
