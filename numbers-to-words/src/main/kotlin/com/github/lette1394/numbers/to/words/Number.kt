7e79b38ba11c0752ff8287482e5b6ec2509b893b
lette1394 <lette1394@gmail.com>
2022-03-02 00:03:01 +0900

    refactor

--- numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
+++ numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
@@ -1,171 +1,38 @@
 package com.github.lette1394.numbers.to.words
 
 class Number(private val value: Long) {
+    private val postfixes = listOf("", "십", "백", "천")
 
     fun print(): String {
-        if (value.toString().length >= 4) {
-            val one = (value / 1000) % 10
-            val two = (value / 100) % 10
-            val three = (value / 10) % 10
-            val four = value % 10
-
-            val one_ = when (one) {
-                0L -> ""
-                1L -> ""
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            } + "천"
-
-            val two_ = when (two) {
-                0L -> ""
-                1L -> "백"
-                2L -> "이백"
-                3L -> "삼백"
-                4L -> "사백"
-                5L -> "오백"
-                6L -> "육백"
-                7L -> "칠백"
-                8L -> "팔백"
-                9L -> "구백"
-                else -> TODO()
-            }
-
-            val three_ = when (three) {
-                0L -> ""
-                1L -> "십"
-                2L -> "이십"
-                3L -> "삼십"
-                4L -> "사십"
-                5L -> "오십"
-                6L -> "육십"
-                7L -> "칠십"
-                8L -> "팔십"
-                9L -> "구십"
-                else -> TODO()
-            }
-
-            val four_ = when (four) {
-                0L -> ""
-                1L -> "일"
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            }
-
-            return "$one_$two_$three_$four_"
+        if (value == 0L) {
+            return "영"
         }
 
-        if (value.toString().length >= 3) {
-            val one = (value / 100) % 10
-            val two = (value / 10) % 10
-            val three = value % 10
+        return value.toString()
+            .toCharArray()
+            .map { it.digitToInt() }
+            .mapIndexed { index, num -> "${digit(num)}${postfix(index)}" }
+            .map { it.replace(Regex("일(.)")) { result -> result.groupValues[1] } }
+            .map { it.replace(Regex("영."), "") }
+            .map { it.replace(Regex("영"), "") }
+            .joinToString("")
+    }
 
-            val one_ = when (one) {
-                0L -> ""
-                1L -> ""
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            } + "백"
+    private fun postfix(index: Int): String {
+        return postfixes[value.toString().length - index - 1]
+    }
 
-            val two_ = when (two) {
-                0L -> ""
-                1L -> "십"
-                2L -> "이십"
-                3L -> "삼십"
-                4L -> "사십"
-                5L -> "오십"
-                6L -> "육십"
-                7L -> "칠십"
-                8L -> "팔십"
-                9L -> "구십"
-                else -> TODO()
-            }
-
-            val three_ = when (three) {
-                0L -> ""
-                1L -> "일"
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            }
-
-            return "$one_$two_$three_"
-        }
-
-
-        if (value.toString().length >= 2) {
-            val up = (value / 10) % 10
-            val down = value % 10
-
-            val first = when (up) {
-                0L -> ""
-                1L -> ""
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            } + "십"
-
-            val second = when (down) {
-                0L -> ""
-                1L -> "일"
-                2L -> "이"
-                3L -> "삼"
-                4L -> "사"
-                5L -> "오"
-                6L -> "육"
-                7L -> "칠"
-                8L -> "팔"
-                9L -> "구"
-                else -> TODO()
-            }
-
-            return first + second
-        }
-
-        return when (value) {
-            0L -> "영"
-            1L -> "일"
-            2L -> "이"
-            3L -> "삼"
-            4L -> "사"
-            5L -> "오"
-            6L -> "육"
-            7L -> "칠"
-            8L -> "팔"
-            9L -> "구"
-            else -> TODO()
-        }
+    private fun digit(num: Int): String = when (num) {
+        0 -> "영"
+        1 -> "일"
+        2 -> "이"
+        3 -> "삼"
+        4 -> "사"
+        5 -> "오"
+        6 -> "육"
+        7 -> "칠"
+        8 -> "팔"
+        9 -> "구"
+        else -> TODO()
     }
 }
