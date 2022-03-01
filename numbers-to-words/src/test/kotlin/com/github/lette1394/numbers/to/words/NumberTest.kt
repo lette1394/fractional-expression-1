af7e85fd4a892dfbb7d81624f7e38793233eead2
lette1394 <lette1394@gmail.com>
2022-03-01 21:51:29 +0900

    red: 1 digit

--- numbers-to-words/src/test/kotlin/com/github/lette1394/numbers/to/words/NumberTest.kt
+++ numbers-to-words/src/test/kotlin/com/github/lette1394/numbers/to/words/NumberTest.kt
@@ -1,18 +1,25 @@
 package com.github.lette1394.numbers.to.words
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class NumberTest : FreeSpec({
-    "0" {
-        Number(0).print() shouldBe "영"
-    }
-
-    "1" {
-        Number(1).print() shouldBe "일"
-    }
-
-    "2" {
-        Number(2).print() shouldBe "이"
+    "1 digit" {
+        forAll(
+            row(0L, "영"),
+            row(1L, "일"),
+            row(2L, "이"),
+            row(3L, "삼"),
+            row(4L, "사"),
+            row(5L, "오"),
+            row(6L, "육"),
+            row(7L, "칠"),
+            row(8L, "팔"),
+            row(9L, "구"),
+        ) { number, word ->
+            Number(number).print() shouldBe word
+        }
     }
 })
