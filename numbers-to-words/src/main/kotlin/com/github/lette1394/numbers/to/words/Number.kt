23d4f3a6e6ea2edc1010ef35dec2407023c5e6d5
lette1394 <lette1394@gmail.com>
2022-03-01 22:04:09 +0900

    green: 2 digits

--- numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
+++ numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
@@ -3,6 +3,41 @@ package com.github.lette1394.numbers.to.words
 class Number(private val value: Long) {
 
     fun print(): String {
+        if (value.toString().length >= 2) {
+            val up = value / 10
+            val down = value % 10
+
+            val first = when (up) {
+                0L -> ""
+                1L -> ""
+                2L -> "이"
+                3L -> "삼"
+                4L -> "사"
+                5L -> "오"
+                6L -> "육"
+                7L -> "칠"
+                8L -> "팔"
+                9L -> "구"
+                else -> TODO()
+            } + "십"
+
+            val second = when (down) {
+                0L -> ""
+                1L -> "일"
+                2L -> "이"
+                3L -> "삼"
+                4L -> "사"
+                5L -> "오"
+                6L -> "육"
+                7L -> "칠"
+                8L -> "팔"
+                9L -> "구"
+                else -> TODO()
+            }
+
+            return first + second
+        }
+
         return when (value) {
             0L -> "영"
             1L -> "일"
