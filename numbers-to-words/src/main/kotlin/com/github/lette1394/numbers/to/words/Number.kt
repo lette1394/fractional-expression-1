3fb5e89d4d3d9bb0abc7dcbe684e720a92716593
lette1394 <lette1394@gmail.com>
2022-03-01 22:28:40 +0900

    green: 3 digits

--- numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
+++ numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
@@ -3,6 +3,57 @@ package com.github.lette1394.numbers.to.words
 class Number(private val value: Long) {
 
     fun print(): String {
+        if (value.toString().length >= 3) {
+            val one = value / 100
+            val two = (value / 10) % 10
+            val three = value % 10
+
+            val one_ = when (one) {
+                0L -> ""
+                1L -> ""
+                2L -> "이"
+                3L -> "삼"
+                4L -> "사"
+                5L -> "오"
+                6L -> "육"
+                7L -> "칠"
+                8L -> "팔"
+                9L -> "구"
+                else -> TODO()
+            } + "백"
+
+            val two_ = when (two) {
+                0L -> ""
+                1L -> "십"
+                2L -> "이십"
+                3L -> "삼십"
+                4L -> "사십"
+                5L -> "오십"
+                6L -> "육십"
+                7L -> "칠십"
+                8L -> "팔십"
+                9L -> "구십"
+                else -> TODO()
+            }
+
+            val three_ = when (three) {
+                0L -> ""
+                1L -> "일"
+                2L -> "이"
+                3L -> "삼"
+                4L -> "사"
+                5L -> "오"
+                6L -> "육"
+                7L -> "칠"
+                8L -> "팔"
+                9L -> "구"
+                else -> TODO()
+            }
+
+            return "$one_$two_$three_"
+        }
+
+
         if (value.toString().length >= 2) {
             val up = value / 10
             val down = value % 10
