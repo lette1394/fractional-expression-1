dd0ff7c903bdf6c4e922abe163d410f623c4debb
lette1394 <lette1394@gmail.com>
2022-03-04 02:02:36 +0900

    green: 6 digits

--- numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
+++ numbers-to-words/src/main/kotlin/com/github/lette1394/numbers/to/words/Number.kt
@@ -1,7 +1,8 @@
 package com.github.lette1394.numbers.to.words
 
 class Number(private val value: Long) {
-    private val postfixes = listOf("", "십", "백", "천", "만")
+    private val postfixes = listOf("", "십", "백", "천")
+    private val postfixes2 = listOf("", "*만*", "*억*")
 
     fun print(): String {
         if (value == 0L) {
@@ -11,15 +12,26 @@ class Number(private val value: Long) {
         return value.toString()
             .toCharArray()
             .map { it.digitToInt() }
-            .mapIndexed { index, num -> "${digit(num)}${postfix(index)}" }
+            .mapIndexed { index, num -> "${digit(num)}${postfix1(index)}${postfix2(index)}" }
             .map { it.replace(Regex("일(.)")) { result -> result.groupValues[1] } }
-            .map { it.replace(Regex("영."), "") }
+            .map { it.replace(Regex("영([가-핳])"), "") }
             .map { it.replace(Regex("영"), "") }
+            .map { it.replace(Regex("\\*"), "") }
             .joinToString("")
     }
 
-    private fun postfix(index: Int): String {
-        return postfixes[value.toString().length - index - 1]
+    private fun postfix1(index: Int): String {
+        val position = (value.toString().length - index - 1)
+        return postfixes[position % 4]
+    }
+
+    private fun postfix2(index: Int): String {
+        val position = (value.toString().length - index - 1)
+        return if (position == 4) {
+            postfixes2[1]
+        } else {
+            ""
+        }
     }
 
     private fun digit(num: Int): String = when (num) {
