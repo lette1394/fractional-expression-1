6a567c33e79efda6a8c3aaba5dceac871df1b7c1
lette1394 <lette1394@gmail.com>
2022-03-04 23:27:07 +0900

    update ValidPasswordTest

--- password-validator-2/src/test/kotlin/com/github/lette1394/password/validator/ValidPasswordTest.kt
+++ password-validator-2/src/test/kotlin/com/github/lette1394/password/validator/ValidPasswordTest.kt
@@ -4,17 +4,30 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class ValidPasswordTest : FreeSpec({
-    "Password must be at least 8 characters: true" {
-        ValidPassword("12345678").valid() shouldBe true
-        ValidPassword("123456789").valid() shouldBe true
-        ValidPassword("123456789abcd").valid() shouldBe true
-    }
+    "Password must be at least 8 characters" - {
+        "input: 1234578" {
+            ValidPassword("12345678").valid() shouldBe true
+            ValidPassword("12345678").violation() shouldBe ""
+        }
 
-    "Password must be at least 8 characters: false" {
-        ValidPassword("").valid() shouldBe false
-        ValidPassword("").violation() shouldBe "Password must be at least 8 characters"
+        "input: 123456789" {
+            ValidPassword("123456789").valid() shouldBe true
+            ValidPassword("123456789").violation() shouldBe ""
+        }
 
-        ValidPassword("1234567").valid() shouldBe false
-        ValidPassword("1234567").violation() shouldBe "Password must be at least 8 characters"
+        "input: 123456789abcd" {
+            ValidPassword("123456789abcd").valid() shouldBe true
+            ValidPassword("123456789abcd").violation() shouldBe "123456789abcd"
+        }
+
+        "input: (empty)" {
+            ValidPassword("").valid() shouldBe false
+            ValidPassword("").violation() shouldBe "Password must be at least 8 characters"
+        }
+
+        "input: 1234567" {
+            ValidPassword("1234567").valid() shouldBe false
+            ValidPassword("1234567").violation() shouldBe "Password must be at least 8 characters"
+        }
     }
 })
