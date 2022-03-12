e000afd27a02a44e61f362553cf44174ced0707d
lette1394 <lette1394@gmail.com>
2022-03-13 01:47:43 +0900

    coffee-machine-1

---
+++ coffee-machine-1/src/main/kotlin/com/github/lette1394/tdd/practices/coffee/machine/App.kt
@@ -0,0 +1,8 @@
+package com.github.lette1394.tdd.practices.coffee.machine
+
+class App {
+}
+
+fun main() {
+    println("hello world!")
+}
---
+++ coffee-machine-1/src/test/kotlin/com/github/lette1394/tdd/practices/coffee/machine/AppKtTest.kt
@@ -0,0 +1,9 @@
+package com.github.lette1394.tdd.practices.coffee.machine
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class AppKtTest : FreeSpec({
+
+
+})
