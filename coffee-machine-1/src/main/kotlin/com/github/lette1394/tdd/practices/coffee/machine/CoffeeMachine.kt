7eb0871eabdd8559e5f42cd7e2a6ae7cf33e5f4c
lette1394 <lette1394@gmail.com>
2022-03-14 01:03:09 +0900

    iteration 1 - passed

--- coffee-machine-1/src/main/kotlin/com/github/lette1394/tdd/practices/coffee/machine/CoffeeMachine.kt
+++ coffee-machine-1/src/main/kotlin/com/github/lette1394/tdd/practices/coffee/machine/CoffeeMachine.kt
@@ -3,6 +3,26 @@ package com.github.lette1394.tdd.practices.coffee.machine
 class CoffeeMachine {
 
     fun translate(order: Order): String {
-        return ""
+        return when (order) {
+            is Tea -> {
+                val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+                val stirring = if (order.sugar > 0) "0" else ""
+
+                "T:$sugar:$stirring"
+            }
+            is Chocolate -> {
+                val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+                val stirring = if (order.sugar > 0) "0" else ""
+
+                "H:$sugar:$stirring"
+            }
+            is Coffee -> {
+                val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+                val stirring = if (order.sugar > 0) "0" else ""
+
+                "C:$sugar:$stirring"
+            }
+            is Message -> "M:${order.contents}"
+        }
     }
 }
