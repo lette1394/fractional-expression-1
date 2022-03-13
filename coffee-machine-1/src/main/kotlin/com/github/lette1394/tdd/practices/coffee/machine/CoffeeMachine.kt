0868ac73dcb0844cf48fbef08d091d2328659d6b
lette1394 <lette1394@gmail.com>
2022-03-14 01:50:39 +0900

    iteration 3 - refactor

--- coffee-machine-1/src/main/kotlin/com/github/lette1394/tdd/practices/coffee/machine/CoffeeMachine.kt
+++ coffee-machine-1/src/main/kotlin/com/github/lette1394/tdd/practices/coffee/machine/CoffeeMachine.kt
@@ -13,46 +13,55 @@ class CoffeeMachine(private val drinkMaker: DrinkMaker) {
 
     private fun translate(order: Order): String {
         return when (order) {
-            is Tea -> {
-                if (remainingCoins >= 400) {
-                    val sugar = if (order.sugar > 0) "${order.sugar}" else ""
-                    val stirring = if (order.sugar > 0) "0" else ""
-                    val extraHot = if (order.extraHot) "h" else ""
-                    return "T$extraHot:$sugar:$stirring"
-                }
-
-                "M:${400 - remainingCoins}원이 부족합니다"
-            }
-            is Chocolate -> {
-                if (remainingCoins >= 500) {
-                    val sugar = if (order.sugar > 0) "${order.sugar}" else ""
-                    val stirring = if (order.sugar > 0) "0" else ""
-                    val extraHot = if (order.extraHot) "h" else ""
-
-                    return "H$extraHot:$sugar:$stirring"
-                }
-
-                "M:${500 - remainingCoins}원이 부족합니다"
-            }
-            is Coffee -> {
-                if (remainingCoins >= 600) {
-                    val sugar = if (order.sugar > 0) "${order.sugar}" else ""
-                    val stirring = if (order.sugar > 0) "0" else ""
-                    val extraHot = if (order.extraHot) "h" else ""
-
-                    return "C$extraHot:$sugar:$stirring"
-                }
-
-                "M:${600 - remainingCoins}원이 부족합니다"
-            }
-            is OrangeJuice -> {
-                if (remainingCoins >= 600) {
-                    return "O::"
-                }
-
-                "M:${600 - remainingCoins}원이 부족합니다"
-            }
+            is Tea -> tea(order)
+            is Chocolate -> chocolate(order)
+            is Coffee -> coffee(order)
+            is OrangeJuice -> orangeJuice()
             is Message -> "M:${order.contents}"
         }
+    }
+
+    private fun orangeJuice(): String {
+        if (remainingCoins >= 600) {
+            return "O::"
+        }
+
+        return "M:${600 - remainingCoins}원이 부족합니다"
+    }
+
+    private fun coffee(order: Coffee): String {
+        if (remainingCoins >= 600) {
+            val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+            val stirring = if (order.sugar > 0) "0" else ""
+            val extraHot = if (order.extraHot) "h" else ""
+
+            return "C$extraHot:$sugar:$stirring"
+        }
+
+        return "M:${600 - remainingCoins}원이 부족합니다"
+    }
+
+    private fun chocolate(order: Chocolate): String {
+        if (remainingCoins >= 500) {
+            val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+            val stirring = if (order.sugar > 0) "0" else ""
+            val extraHot = if (order.extraHot) "h" else ""
+
+            return "H$extraHot:$sugar:$stirring"
+        }
+
+        return "M:${500 - remainingCoins}원이 부족합니다"
+    }
+
+    private fun tea(order: Tea): String {
+        if (remainingCoins >= 400) {
+            val sugar = if (order.sugar > 0) "${order.sugar}" else ""
+            val stirring = if (order.sugar > 0) "0" else ""
+            val extraHot = if (order.extraHot) "h" else ""
+
+            return "T$extraHot:$sugar:$stirring"
+        }
+
+        return "M:${400 - remainingCoins}원이 부족합니다"
     }
 }
