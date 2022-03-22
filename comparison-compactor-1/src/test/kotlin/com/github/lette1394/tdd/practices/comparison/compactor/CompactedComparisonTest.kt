689ea2978087cc5036f91956181f6e40267b0616
lette1394 <lette1394@gmail.com>
2022-03-23 03:32:10 +0900

    test: fail ba compare bc

--- comparison-compactor-1/src/test/kotlin/com/github/lette1394/tdd/practices/comparison/compactor/CompactedComparisonTest.kt
+++ comparison-compactor-1/src/test/kotlin/com/github/lette1394/tdd/practices/comparison/compactor/CompactedComparisonTest.kt
@@ -7,4 +7,8 @@ class CompactedComparisonTest : FreeSpec({
     "b compare c" {
         CompactedComparison(0, "b", "c").toString() shouldBe "expected:<[b]>, but was:<[c]>"
     }
+
+    "ba compare bc" {
+        CompactedComparison(1, "ba", "bc").toString() shouldBe "expected:<b[a]>, but was:<b[c]>"
+    }
 })
